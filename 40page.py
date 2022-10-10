787d7d1714537bf50d74475fa39f37f78cedee63
grad154 <114381790+grad154@users.noreply.github.com>
2022-10-10 09:58:59 +0700

    Update 40page.py

--- 40page.py
+++ 40page.py
@@ -1,7 +1,8 @@
+#перевод из десятичной системы счисления в любую другую
 num = int(input('enter the num(Np): '))
 ns = int(input('enter number system: '))
 res = 0
-y =0
+y = 0
 while num > 0:
    res = res +(num % 10)*(ns**y)
    y += 1
