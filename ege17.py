d14fd36db5ab284ab2949678161b22d96a03a438
grad154 <114381790+grad154@users.noreply.github.com>
2022-10-17 11:01:18 +0700

    Add files via upload

---
+++ ege17.py
@@ -0,0 +1,7 @@
+for x in range(2):
+   for y in range(2):
+      for z in range(2):
+         for w in range(2):
+            if (not(y<=x)or(z<=w)or not(z)) == 0:
+               print(x, y, z, w)
+            
