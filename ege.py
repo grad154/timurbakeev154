80aa1266f0f3007cb6e1b0c0e96ff356621a8b6e
grad154 <114381790+grad154@users.noreply.github.com>
2022-10-24 16:31:56 +0700

    Update ege.py

--- ege.py
+++ ege.py
@@ -1,4 +1,4 @@
-import math
+#n = int(input("n="))
 n = 1000
 lst = [1]
 for i in range(2, n+1):
@@ -9,23 +9,21 @@ for i in range(2, n+1):
     else:
         lst.append(i)
 
-print(lst)
+#print(lst)
 Str1 = '>' + 39 * '0' + n * '1' + 39 * '2'
 
 for n in range(1, 1000):
   Str1 = '>' + 39 * '0' + n * '1' + 39 * '2'
-  print(Str1)
-  while ('>1' in Str1) or ('>2' in Str1) or ('>0' in Str1):
+  while '>1' in Str1 or '>2' in Str1 or '>0' in Str1:
     if '>1' in Str1:
-      Str1.replace('>1', '22>')
+      Str1 = Str1.replace('>1', '22>')
     if '>2' in Str1:
-      Str1.replace('>2', '2>')
+      Str1 = Str1.replace('>2', '2>')
     if '>0' in Str1:
-      Str1.replace('>0', '1>')
+      Str1 = Str1.replace('>0', '1>')
   spisok1 = list(Str1)
-  print(spisok1)
-  for g in lst:
-    print(g)
-    if g == sum(spisok1[:-1]):
+  for g in range(len(lst)):
+    print(lst[g])
+    input()
+    if (lst[g]) == sum(spisok1[0,len(spisok1 - 1)]):
       print(n)
-      break
