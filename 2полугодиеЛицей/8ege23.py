4bc80c8c618777c295b2b1a65ac97992b24acacf
grad154 <114381790+grad154@users.noreply.github.com>
2023-01-16 13:38:40 +0700

    Add files via upload

---
+++ 2полугодиеЛицей/8ege23.py
@@ -0,0 +1,32 @@
+from itertools import product
+# count = 0
+# for i in range(4):
+#     for j in range(4):
+#         for k in range(4):
+#             print(str(i)+str(j)+str(k))
+#             count += 1
+
+# print("num is: ", count)
+
+from itertools import product
+nums=product('01234567',repeat=5)
+k=0
+n='16 36 56 76 61 63 65 67'
+nn=n.split()    
+for n in nums:
+    numb=''.join(n)
+    sp=[]
+    if numb.count('6')==1 and numb[0]!='0':
+        for x in nn:
+            if x in numb:
+                sp.append(x)
+        if not sp: 
+            k+=1
+print(k)
+
+
+
+
+
+
+
